4c38378fa137a54f747a997e561062cd2c00750b
StevenRCE0 <40051361+StevenRCE0@users.noreply.github.com>
2022-12-24 15:21:18 +0800

    Added more stuff

---
+++ shell/app/layout.tsx
@@ -0,0 +1,24 @@
+import {CSSProperties} from "react";
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+    let contentBlockStyle = {
+        '--contentBlockHeight': '100vh',
+        '--contentBlockWidth': '100vw'
+    } as CSSProperties
+    return (
+        <html>
+        <head>
+            <title>鱼网 FishNet</title>
+        </head>
+        <body style={{backgroundColor: 'rgb(243, 242, 246)', height: '100vh'}}>
+        <div style={contentBlockStyle}>
+            {children}
+        </div>
+        </body>
+        </html>
+    );
+}
