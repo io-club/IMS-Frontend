fb52cd8d7b3907e1f56b843e78bf2a6e766f2c10
StevenRCE0 <40051361+StevenRCE0@users.noreply.github.com>
2022-12-25 00:13:00 +0800

    MDC

--- shell/app/components/SlugButton.tsx
+++ shell/app/components/SlugButton.tsx
@@ -1,12 +1,23 @@
-import React from "react";
+'use client'
+
+import React, {useEffect, useRef} from "react";
 import buttonStyle from './Buttons.module.css'
+import {MDCRipple} from '@material/ripple/index';
+import './buttons.scss'
+
 
 type ButtonProps = {
     children: React.ReactNode
 }
 
 export default function SlugButton(props: ButtonProps) {
-    return <button className={buttonStyle.SlugButton}>
+    const buttonRef = useRef<HTMLButtonElement>(null)
+    useEffect(() => {
+        MDCRipple.attachTo(buttonRef.current!)
+    })
+    return <button className={['mdc-button', 'mdc-button--unelevated', buttonStyle.SlugButton].join(' ')} ref={buttonRef}>
         {props.children}
+        <span className="mdc-button__ripple"></span>
+        <span className="mdc-button__focus-ring"></span>
     </button>
 }
